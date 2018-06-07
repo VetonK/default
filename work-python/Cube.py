ea0c94a30f2a97bfa7d7c9ca83d90c8f1c141bfd
VetonK <40038989+VetonK@users.noreply.github.com>
2018-06-07 22:54:49 +0200

    update

--- work-python/Cube.py
+++ work-python/Cube.py
@@ -7,25 +7,37 @@ Created on Wed Jun  6 19:36:29 2018
 
 #
 import sys
-nbr=5
-cube=[[' ',' ','+','-','-','-','+'],[' ','/',' ',' ',' ','/','|'],['+','-','-','-','+',' ','|'],['|',' ',' ', ' ','|',' ','+'],['|',' ',' ',' ','|','/'],['+','-','-','-','+',]]
- 
-#liste =[]
-def imprime_cube(cube):
-    i=j=0
-    while(j<len(cube)):
-        i=0
-        while(i<len(cube[j])):
-            print(cube[j][i],end='')
-            i=i+1
 
-        print()
-        j=j+1
+nbr=5
+cube=[['+','-','-','-','+'],['/',' ',' ',' ','/','|'],['+','-','-','-','+',' ','|'],['|',' ',' ', ' ','|',' ','+'],['|',' ',' ',' ','|','/'],['+','-','-','-','+',]]
+surface= [['#'] * 20]*20
+
+def imprime_cube(surface):
+  for j in range(len(surface)):
+            for i in range(len(surface[j])):
+                print(surface[j][i],end='')
+            print('')
+  return
+
+
+def ajoute_cube(z):
+    for x in range(z):
+        for j in range(len(cube)):
+            for i in range(len(cube[j])):
+                print(cube[j][i],end='')
+                surface[j][i]=cube[j][i]
+            print()
+    return
+def imprime_multi_cube():
     return
 
 
-def imprime_multi_cube():
-    print('hello')
-    
-imprime_multi_cube()
+imprime_cube(surface)
+print('hello')
+print(cube)
+ajoute_cube(1)
+print(surface)
+imprime_cube(surface)
+
+#imprime_multi_cube()
 
